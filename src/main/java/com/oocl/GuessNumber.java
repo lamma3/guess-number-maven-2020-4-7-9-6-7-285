7ea9eecbae77702a46a6597680844aea0ce29fb9
Marco Lam <lamma3@oocl.com>
2020-04-08 18:20:38 +0800

    [Marco] pass test case test_calculateFeedback_when_noCorrect_then_return0A0B

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -5,6 +5,17 @@ import java.util.List;
 public class GuessNumber {
 
     public String calculateFeedback(List<Integer> answer, List<Integer> guess) {
-        return null;
+        int numOfCorrect = getNumberOfCorrectNumber(answer, guess);
+        return formatResult(numOfCorrect);
+    }
+
+    private int getNumberOfCorrectNumber(List<Integer> answer, List<Integer> guess) {
+        return (int) guess.stream()
+                .filter(answer::contains)
+                .count();
+    }
+
+    private String formatResult(int numOfCorrect) {
+        return String.format("0A%dB", numOfCorrect);
     }
 }
