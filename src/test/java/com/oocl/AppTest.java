394c85fb12ba7df9475c65e1e56344fd48627a4f
Marco Lam <lamma3@oocl.com>
2020-04-08 23:03:16 +0800

    [Marco] add test case test_play_when_input_then_returnResult

--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -1,13 +1,31 @@
 package com.oocl;
 
 import com.oocl.exception.GuessNumberGameOverException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     private void input(String str) {
         InputStream inputStream = new ByteArrayInputStream(str.getBytes());
@@ -20,7 +38,7 @@ public class AppTest {
         Mockito.when(calculator.isWin(Mockito.anyString(), Mockito.anyInt()))
                 .thenReturn(true);
 
-        input("1 2 3 4");
+        input("1 2 3 4" + System.lineSeparator());
 
         App.play(calculator, new Generator());
     }
@@ -44,5 +62,23 @@ public class AppTest {
                 "1 2 3 4" + System.lineSeparator());
 
         App.play(calculator, new Generator());
+    }
+
+    @Test
+    public void test_play_when_input_then_returnResult() throws Exception {
+        Calculator calculator = Mockito.mock(Calculator.class);
+        Mockito.when(calculator.calculateFeedback(Mockito.anyListOf(Integer.class), Mockito.anyListOf(Integer.class)))
+                .thenReturn("4A0B");
+        Mockito.when(calculator.isWin(Mockito.anyString(), Mockito.anyInt()))
+                .thenReturn(true);
+
+        input("1 2 3 4" + System.lineSeparator());
+
+
+        try {
+            App.play(calculator, new Generator());
+        } catch (GuessNumberGameOverException e) {
+            Assert.assertEquals("4A0B" + System.lineSeparator() ,outContent.toString());
+        }
     }
 }
