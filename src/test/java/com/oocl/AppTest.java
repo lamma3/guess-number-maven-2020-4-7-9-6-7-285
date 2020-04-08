90e6e23371381395c8bfa4433e1acfcc57ea31ef
Marco Lam <lamma3@oocl.com>
2020-04-08 21:44:15 +0800

    [Marco] add test case test_play_when_win_then_throwGuessNumberGameOverException

--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -1,10 +1,28 @@
 package com.oocl;
 
+import com.oocl.exception.GuessNumberGameOverException;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class AppTest {
-    @Test
-    public void helloWorld() {
 
+    private void input(String str) {
+        InputStream inputStream = new ByteArrayInputStream(str.getBytes());
+        System.setIn(inputStream);
+    }
+
+    @Test(expected = GuessNumberGameOverException.class)
+    public void test_play_when_win_then_throwGuessNumberGameOverException() throws Exception {
+        Calculator calculator = Mockito.mock(Calculator.class);
+        Generator generator = Mockito.mock(Generator.class);
+        Mockito.when(calculator.isWin(Mockito.anyString(), Mockito.anyInt()))
+                .thenReturn(true);
+
+        App app = new App();
+        input("1 2 3 4");
+        app.play(calculator, generator);
     }
 }
