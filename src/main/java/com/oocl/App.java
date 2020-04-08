3c5e0b3f3bd9c8dd9b9c8b66ff23e948f3f6c51e
Marco Lam <lamma3@oocl.com>
2020-04-08 22:28:36 +0800

    [Marco] pass test case test_play_when_win_then_throwGuessNumberGameOverException

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -1,16 +1,44 @@
 package com.oocl;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.oocl.exception.GuessNumberDuplicateNumberException;
+import com.oocl.exception.GuessNumberGameOverException;
+import com.oocl.exception.GuessNumberInputSizeNotMatchException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+
+    private final static int NUMBER_LIST_SIZE = 4;
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
-    public void play(Calculator calculator, Generator generator) {
+    public void play(Calculator calculator, Generator generator) throws GuessNumberGameOverException, GuessNumberDuplicateNumberException, GuessNumberInputSizeNotMatchException {
+        boolean gameover = false;
+        List<Integer> answer = generator.generateAnswer(NUMBER_LIST_SIZE);
+        while (!gameover) {
+            String guess = scanner.nextLine();
+            List<Integer> guessNumberList = parseGuess(guess);
+            String result = calculator.calculateFeedback(answer, guessNumberList);
+            if (calculator.isWin(result, NUMBER_LIST_SIZE)) {
+                gameover = true;
+            }
+        }
+
+        throw new GuessNumberGameOverException();
+    }
+
+    private List<Integer> parseGuess(String guess) {
+        String[] guessSplitBySpace = guess.split(" ");
+        List<Integer> numberList = new ArrayList<>();
+        for (String numString: guessSplitBySpace) {
+            int num = Integer.parseInt(numString);
+            numberList.add(num);
+        }
+        return numberList;
     }
 }
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -17,12 +17,12 @@ public class AppTest {
     @Test(expected = GuessNumberGameOverException.class)
     public void test_play_when_win_then_throwGuessNumberGameOverException() throws Exception {
         Calculator calculator = Mockito.mock(Calculator.class);
-        Generator generator = Mockito.mock(Generator.class);
         Mockito.when(calculator.isWin(Mockito.anyString(), Mockito.anyInt()))
                 .thenReturn(true);
 
-        App app = new App();
         input("1 2 3 4");
-        app.play(calculator, generator);
+
+        App app = new App();
+        app.play(calculator, new Generator());
     }
 }
