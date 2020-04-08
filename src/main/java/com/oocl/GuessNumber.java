1f347d05ca3a51123331ed1d71d14e219330ada0
Marco Lam <lamma3@oocl.com>
2020-04-08 18:30:18 +0800

    [Marco] pass test case test_calculateFeedback_when_1CorrectPosition_then_return1A0B

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -1,12 +1,14 @@
 package com.oocl;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GuessNumber {
 
     public String calculateFeedback(List<Integer> answer, List<Integer> guess) {
-        int numOfCorrect = getNumberOfCorrectNumber(answer, guess);
-        return formatResult(numOfCorrect);
+        int numOfCorrectNumber = getNumberOfCorrectNumber(answer, guess);
+        int numOfCorrectPosition = getNumberOfCorrectPosition(answer, guess);
+        return formatResult(numOfCorrectPosition, numOfCorrectNumber - numOfCorrectPosition);
     }
 
     private int getNumberOfCorrectNumber(List<Integer> answer, List<Integer> guess) {
@@ -15,7 +17,13 @@ public class GuessNumber {
                 .count();
     }
 
-    private String formatResult(int numOfCorrect) {
-        return String.format("0A%dB", numOfCorrect);
+    private int getNumberOfCorrectPosition(List<Integer> answer, List<Integer> guess) {
+        return (int) IntStream.range(0, guess.size())
+                .filter(index -> guess.get(index).equals(answer.get(index)))
+                .count();
+    }
+
+    private String formatResult(int numOfCorrectPosition, int numOfWrongPosition) {
+        return String.format("%dA%dB", numOfCorrectPosition, numOfWrongPosition);
     }
 }
