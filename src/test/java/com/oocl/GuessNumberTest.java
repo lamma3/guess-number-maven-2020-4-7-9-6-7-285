130a40fd265d901080513315a15eb2e089700b13
Marco Lam <lamma3@oocl.com>
2020-04-08 18:47:28 +0800

    [Marco] remove duplicate number in test cases

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -12,16 +12,16 @@ public class GuessNumberTest {
     @Test
     public void test_calculateFeedback_when_noCorrect_then_return0A0B() throws Exception {
         List<Integer> answer = new ArrayList<>();
-        answer.add(0);
-        answer.add(0);
-        answer.add(0);
-        answer.add(0);
+        answer.add(1);
+        answer.add(2);
+        answer.add(3);
+        answer.add(4);
 
         List<Integer> guess = new ArrayList<>();
-        guess.add(1);
-        guess.add(1);
-        guess.add(1);
-        guess.add(1);
+        guess.add(5);
+        guess.add(6);
+        guess.add(7);
+        guess.add(8);
 
         GuessNumber guessNumber = new GuessNumber();
         String result = guessNumber.calculateFeedback(answer, guess);
@@ -31,16 +31,16 @@ public class GuessNumberTest {
     @Test
     public void test_calculateFeedback_when_1CorrectButWrongPosition_then_return0A1B() throws Exception {
         List<Integer> answer = new ArrayList<>();
-        answer.add(0);
-        answer.add(0);
-        answer.add(0);
+        answer.add(1);
         answer.add(2);
+        answer.add(3);
+        answer.add(4);
 
         List<Integer> guess = new ArrayList<>();
+        guess.add(5);
+        guess.add(6);
+        guess.add(7);
         guess.add(1);
-        guess.add(1);
-        guess.add(1);
-        guess.add(0);
 
         GuessNumber guessNumber = new GuessNumber();
         String result = guessNumber.calculateFeedback(answer, guess);
@@ -50,16 +50,16 @@ public class GuessNumberTest {
     @Test
     public void test_calculateFeedback_when_1CorrectPosition_then_return1A0B() throws Exception {
         List<Integer> answer = new ArrayList<>();
-        answer.add(0);
-        answer.add(0);
-        answer.add(0);
-        answer.add(0);
+        answer.add(1);
+        answer.add(2);
+        answer.add(3);
+        answer.add(4);
 
         List<Integer> guess = new ArrayList<>();
         guess.add(1);
-        guess.add(1);
-        guess.add(1);
-        guess.add(0);
+        guess.add(6);
+        guess.add(7);
+        guess.add(8);
 
         GuessNumber guessNumber = new GuessNumber();
         String result = guessNumber.calculateFeedback(answer, guess);
@@ -69,11 +69,11 @@ public class GuessNumberTest {
     @Test(expected = GuessNumberInputSizeNotMatchException.class)
     public void test_calculateFeedback_when_inputSizeNotMatch_then_throwGuessNumberInputSizeNotMatchException() throws Exception {
         List<Integer> answer = new ArrayList<>();
-        answer.add(0);
+        answer.add(1);
 
         List<Integer> guess = new ArrayList<>();
-        guess.add(0);
-        guess.add(0);
+        guess.add(1);
+        guess.add(2);
 
         GuessNumber guessNumber = new GuessNumber();
         guessNumber.calculateFeedback(answer, guess);
