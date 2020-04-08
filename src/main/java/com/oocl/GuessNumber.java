a67da8c1744862cb2a0ebf9224e4345e3ff2d3e0
Marco Lam <lamma3@oocl.com>
2020-04-08 18:20:17 +0800

    [Marco] add test case test_calculateFeedback_when_noCorrect_then_return0A0B

---
+++ src/main/java/com/oocl/GuessNumber.java
@@ -0,0 +1,10 @@
+package com.oocl;
+
+import java.util.List;
+
+public class GuessNumber {
+
+    public String calculateFeedback(List<Integer> answer, List<Integer> guess) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -0,0 +1,29 @@
+package com.oocl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuessNumberTest {
+
+    @Test
+    public void test_calculateFeedback_when_noCorrect_then_return0A0B() {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(0);
+        answer.add(0);
+        answer.add(0);
+        answer.add(0);
+
+        List<Integer> guess = new ArrayList<>();
+        guess.add(1);
+        guess.add(1);
+        guess.add(1);
+        guess.add(1);
+
+        GuessNumber guessNumber = new GuessNumber();
+        String result = guessNumber.calculateFeedback(answer, guess);
+        Assert.assertEquals("0A0B", result);
+    }
+}
