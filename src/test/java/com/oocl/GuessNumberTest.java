57ecd54403149feab0af7a7df032f16a8c6a4a41
Marco Lam <lamma3@oocl.com>
2020-04-08 18:21:50 +0800

    [Marco] add and pass test case test_calculateFeedback_when_1CorrectButWrongPosition_then_return0A1B

--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -26,4 +26,23 @@ public class GuessNumberTest {
         String result = guessNumber.calculateFeedback(answer, guess);
         Assert.assertEquals("0A0B", result);
     }
+
+    @Test
+    public void test_calculateFeedback_when_1CorrectButWrongPosition_then_return0A1B() {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(0);
+        answer.add(0);
+        answer.add(0);
+        answer.add(2);
+
+        List<Integer> guess = new ArrayList<>();
+        guess.add(1);
+        guess.add(1);
+        guess.add(1);
+        guess.add(0);
+
+        GuessNumber guessNumber = new GuessNumber();
+        String result = guessNumber.calculateFeedback(answer, guess);
+        Assert.assertEquals("0A1B", result);
+    }
 }
